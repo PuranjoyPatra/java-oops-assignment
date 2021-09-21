359eb6c0c079275c7dd004cd1f3d70706713bd20
Puranjoy Patra <purunjoypatra1997@gmail.com>
2021-09-21 10:28:45 +0530

    modify access specifier in 'Inheritance' class.

--- Inheritance.java
+++ Inheritance.java
@@ -24,12 +24,12 @@ class B extends A
     }
 }
 
-Public class Inheritance
+public class Inheritance
 {
     public static void main(String []args)
     {
         B obj=new B();
-        obj.setData(4);
+        obj.setData(5);
         obj.getData();
     }
 }
